2e2c5fe03f2adc381c2437ecec0e45234bf1c823
Andrew Wu <a-wu.1@onu.edu>
2024-04-10 22:40:20 -0400

    completed hasRun

--- Project23/FileName.cpp
+++ Project23/FileName.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 void displayRun(int values[], int size);
+bool hasRun(int values[], int size);
+bool Run = false;
 int main()
 {
 	srand(time(0));
@@ -8,23 +10,41 @@ int main()
 	int dice[size];
 	for (int i = 0; i < size; i++)
 	{
-		dice[i] = rand() % 6 + 1;
-
+		dice[i] = (rand() % 6) + 1;
 	}
 	displayRun(dice, size);
+	hasRun(dice, size);
+	if (Run = true)
+	{
+		cout << "RAN";
+	}
+	else
+	{
+		cout << "NO RAN";
+	}
 }
 void displayRun(int values[], int size)
 {
-	for (int i = 0; i <= size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (values[i] == values[i + 1])
 		{
 			cout << "(" << values[i] << " " << values[i + 1] << ") ";
 			i = i + 2;
 		}
-		if(values[i] != values[i+1])
+		if (values[i] != values[i + 1])
 		{
 			cout << values[i] << " ";
 		}
+	}
+}
+bool hasRun(int values[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (values[i] == values[i + 1])
+		{
+			Run == true;
+		}return Run;
 	}
 }
